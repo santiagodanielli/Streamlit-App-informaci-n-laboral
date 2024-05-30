7eaf3fc44a30f678c7daa56bcec6de74e2e4e5bb
Santiago <139587626+santiagodanielli@users.noreply.github.com>
2024-05-30 11:31:55 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ descripcion_personal = """
 Soy analista de datos con orientación en investigación social, de mercados, opinión pública y segmentación demográfica. 
 Me dedico a combinar mi formación y experiencia en estadística descriptiva e inferencial con herramientas como Power BI y Python para 
 analizar datos que aporten a la toma de decisiones.
-
-Mi principal interés es continuar desarrollando mis habilidades y experiencia laboral en el análisis estadístico de datos, ciencia de datos y machine larning.
 """
 
 concepto_ad = """
